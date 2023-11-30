8e950086b222e577cd84b8340e283f828a4b72ac
Lucas.Kolanz <kolanzl@oregonstate.edu>
2023-11-30 11:54:47 -0800

    Project 1 functionality all added and working.

--- SciVis/Project.cpp
+++ SciVis/Project.cpp
@@ -34,14 +34,43 @@ void GLWidget::algorithm2() //toggle field/contour line height
 	}
 	update();
 }
-void GLWidget::algorithm3() {
-
+void GLWidget::algorithm3() 
+{
+	if (rainbowMapOn)
+	{
+		vertexColor();
+	}
+	else
+	{
+		rainbowMap();
+	}
+	toggleMapBool(rainbow);
+	update();
 }
-void GLWidget::algorithm4() {
-	
+void GLWidget::algorithm4() 
+{
+	if (bicolorMapOn)
+	{
+		vertexColor();
+	}
+	else
+	{
+		bicolorMap();
+	}
+	toggleMapBool(bicolor);
+	update();
 }
 void GLWidget::algorithm5() {
-	qDebug() << 5;
+	if (greyscaleMapOn)
+	{
+		vertexColor();
+	}
+	else
+	{
+		greyscaleMap();
+	}
+	toggleMapBool(greyscale);
+	update();
 }
 void GLWidget::algorithm6() {
 	qDebug() << 6;
@@ -53,50 +82,25 @@ void GLWidget::algorithm8() {
 	qDebug() << 8;
 }
 
-void drawAllLines()
+///////////////////////////////////////////////////////////////
+////////Start Project 1 functions and helpers
+///////////////////////////////////////////////////////////////
+
+void toggleMapBool(int map)
 {
-	int tot_lines = 100;
-	double max, min, range, value;
-	findMinMax(min, max);
-	range = max - min;
-	range = max - min;
-	for (int i = 0; i < tot_lines; ++i)
+	int arrlen = sizeof(colorMapBools)/sizeof(colorMapBools[0]);
+	for (int i = 0; i < arrlen; ++i)
 	{
-		value = (i * 1.0) * (range / tot_lines) + min;
-		drawContourLine(value, icVector3(0.0,0.0,0.0), false);
+		if (i == map)
+		{
+			*colorMapBools[i] = !*colorMapBools[i];
+		}
+		else
+		{
+			*colorMapBools[i] = false;
+		}
 	}
 }
-
-// void contHeight()
-// {	
-// 	double max, min;
-// 	findMinMax(min, max);
-// 	for (auto& polyline : polylines) 
-// 	{
-// 		for (auto& vertex : polyline.m_vertices)
-// 		{
-// 			// auto& vertex = polylines->vlist[i];
-// 			if (contHeightOn)
-// 			{
-// 				// polyline.m_vertex->z = 0; 
-// 				vertex.z = 0;
-// 			}
-// 			else
-// 			{
-// 				double s_v = 10;//vertex.scalar;
-
-// 				double l = (s_v - min) / (max - min);
-// 				// polyline.vertex->z = l*10; 
-// 				vertex.z = l*10;
-// 			}
-// 		}
-// 	}
-// 	// for (auto i = 0; i < polylines->nverts; i++)
-// 	// {
-		
-// 	// }
-// 	contHeightOn = !contHeightOn;
-// }
 
 void fieldHeight()
 {
@@ -120,19 +124,6 @@ void fieldHeight()
 	fieldHeightOn = !fieldHeightOn;
 }
 
-
-void drawContourLine(double v, icVector3 rgb,int saddle_index)
-{
-	std::list<POLYLINE> edges0;
-	marchingSquare(edges0, *poly, v,saddle_index);
-	std::vector<POLYLINE> polylines0;
-	makePolylineFromEdges(polylines0, edges0);
-	for (auto& polyline : polylines0) {
-		polyline.m_rgb = icVector3(rgb.x, rgb.y, rgb.z);
-		polylines.push_back(polyline);
-	}
-}
-
 void findMinMax(double& min, double& max)
 {
 	min = INFINITY;
@@ -148,5 +139,242 @@ void findMinMax(double& min, double& max)
 		{
 			max = vertex->scalar;
 		}
+	}
+}
+
+void rainbowMap()
+{
+	for (auto i = 0; i < poly->nverts; i++)
+	{
+		auto& vertex = poly->vlist[i];
+		double s_v = vertex->scalar;
+
+		icVector3 c = rainbowColorFromScalar(s_v);
+
+		vertex->R = c.x;
+		vertex->G = c.y;
+		vertex->B = c.z;
+	}
+}
+
+void bicolorMap()
+{
+	double max, min;
+	findMinMax(min, max);
+	for (auto i = 0; i < poly->nverts; i++)
+	{
+		auto& vertex = poly->vlist[i];
+		double s_v = vertex->scalar;
+		icVector3 c1(0.0, 1.0, 0.0);
+		icVector3 c2(1.0, 0.0, 1.0);
+
+		double l = (s_v - min) / (max - min);
+		double r = (max - s_v) / (max - min);
+
+		icVector3 c = c1 * l + c2 * r;
+
+		vertex->R = c.x;
+		vertex->G = c.y;
+		vertex->B = c.z;
+
+	}
+}
+
+void greyscaleMap()
+{
+	double max, min;
+	//TEST ME
+	findMinMax(min, max);
+
+	for (auto i = 0; i < poly->nverts; i++)
+	{
+		auto& vertex = poly->vlist[i];
+		double s_v = vertex->scalar;
+		double gray_ = (s_v - min) / (max - min);
+
+		vertex->R = vertex->G = vertex->B = gray_;
+	}
+}
+
+icVector3 rainbowColorFromScalar(double scalar)
+{
+	icVector3 HSV, c;
+	double max, min;
+	findMinMax(min, max);
+	double delta = max - min;
+	//This value needs to be linearly scaled from 0-300 from the delta value
+	//Don't go all the way to 300 because we want red to blue and 300-360 is magenta
+	HSV.x = (scalar - min) * 300 / delta;
+	HSV.y = 1;
+	HSV.z = 1;
+
+	//NEED TO IMPLEMENT
+	HSVtoRGB(HSV, c);
+	return c;
+}
+
+
+void RGBtoHSV(icVector3& rgb, icVector3& hsv)
+{
+	double cmax,cmin,delta,R,G,B,H,S,V;
+
+	R = rgb.x;
+	G = rgb.y;
+	B = rgb.z;
+	H = 0;
+	S = 0;
+	V = 0;
+	
+	cmax = max(R, G, B);
+	cmin = min(R, G, B);
+
+	delta = cmax - cmin;
+
+	if (delta == 0)
+	{
+		H = 0;
+	}
+	else if (cmax == R)
+	{
+		H = fmod((((G - B) / delta) + 360) * 60, 360);
+	}
+	else if (cmax == G)
+	{
+		H = fmod((((B - R) / delta) + 120) * 60, 360);
+	}
+	else if (cmax == B)
+	{
+		H = fmod((((R - G) / delta) + 240) * 60, 360);
+	}
+	
+	if (cmax == 0)
+	{
+		S = 0;
+	}
+	else
+	{
+		S = delta / cmax;
+	}
+
+	V = cmax;
+
+	hsv.x = H;
+	hsv.y = S;
+	hsv.z = V;
+
+	return;
+}
+
+void HSVtoRGB(icVector3& hsv, icVector3& rgb)
+{
+	double H, S, V,c,x,m,r,g,b;
+
+	H = hsv.x;
+	S = hsv.y;
+	V = hsv.z;
+
+	c = V * S;
+	x = c * (1.0 - fabs(fmod(H / 60.0, 2.0) - 1.0));
+	m = V - c;
+
+
+
+	if (H >= 0.0 && H < 60.0)
+	{
+		r = c;
+		g = x;
+		b = 0;
+	}
+	else if (H >= 60.0 && H < 120.0)
+	{
+		r = x;
+		g = c;
+		b = 0;
+	}
+	else if (H >= 120.0 && H < 180.0)
+	{
+		r = 0;
+		g = c;
+		b = x;
+	}
+	else if (H >= 180.0 && H < 240.0)
+	{
+		r = 0;
+		g = x;
+		b = c;
+	}
+	else if (H >= 240.0 && H < 300.0)
+	{
+		r = x;
+		g = 0;
+		b = c;
+	}
+	else if (H >= 300.0 && H < 360.0)
+	{
+		r = c;
+		g = 0;
+		b = x;
+	}
+	else
+	{
+		std::cerr << "ERROR: H needs to be between 0 and 360 but is "<<H<<'.' << std::endl;
+		r = -1;
+		g = -1;
+		b = -1;
+	}
+
+	rgb.x = r + m;
+	rgb.y = g + m;
+	rgb.z = b + m;
+
+	return;
+}
+
+double max(double m1,double m2,double m3)
+{
+	return std::max(m1, std::max(m2, m3));
+}
+
+double min(double m1, double m2, double m3)
+{
+	return std::min(m1, std::min(m2, m3));
+}
+
+///////////////////////////////////////////////////////////////
+////////End Project 1 functions and helpers
+///////////////////////////////////////////////////////////////
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+////////Start Project 2 functions and helpers
+///////////////////////////////////////////////////////////////
+
+void drawAllLines()
+{
+	int tot_lines = 100;
+	double max, min, range, value;
+	findMinMax(min, max);
+	range = max - min;
+	range = max - min;
+	for (int i = 0; i < tot_lines; ++i)
+	{
+		value = (i * 1.0) * (range / tot_lines) + min;
+		drawContourLine(value, icVector3(0.0,0.0,0.0), false);
+	}
+}
+
+void drawContourLine(double v, icVector3 rgb,int saddle_index)
+{
+	std::list<POLYLINE> edges0;
+	marchingSquare(edges0, *poly, v,saddle_index);
+	std::vector<POLYLINE> polylines0;
+	makePolylineFromEdges(polylines0, edges0);
+	for (auto& polyline : polylines0) {
+		polyline.m_rgb = icVector3(rgb.x, rgb.y, rgb.z);
+		polylines.push_back(polyline);
 	}
 }
--- SciVis/Project.h
+++ SciVis/Project.h
@@ -2,9 +2,29 @@
 
 bool fieldHeightOn = false;
 bool contHeightOn = false;
+bool rainbowMapOn = false;
+bool greyscaleMapOn = false;
+bool bicolorMapOn = false;
+
+enum colorMaps {rainbow,bicolor,greyscale};
+bool* colorMapBools[] = {&rainbowMapOn,&bicolorMapOn,&greyscaleMapOn};
 
 void drawContourLine(double v, icVector3 rgb,int saddle_index);
-void findMinMax(double& min, double& max);
+icVector3 rainbowColorFromScalar(double scalar);
+
 // void contHeight();
 void fieldHeight();
 void drawAllLines();
+
+void rainbowMap();
+void greyscaleMap();
+void bicolorMap();
+
+void toggleMapBool(int map);
+
+void HSVtoRGB(icVector3& hsv, icVector3& rgb);
+void RGBtoHSV(icVector3& rgb, icVector3& hsv);
+
+void findMinMax(double& min, double& max);
+double max(double m1,double m2,double m3);
+double min(double m1, double m2,double m3);
